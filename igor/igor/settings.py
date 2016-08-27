84ccf7251b49fe28d99ac2e9fc3818efd0ed15f8
Sotaya <sotayamy@gmail.com>
2016-08-27 11:08:48 +0200

    Tweaks landing

--- igor/igor/settings.py
+++ igor/igor/settings.py
@@ -130,5 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_base")
+
 CODEMIRROR_PATH = 'js/codemirror'
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- igor/igor/urls.py
+++ igor/igor/urls.py
@@ -17,9 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
-
 urlpatterns = [
     url(r'^', include('puzzles.urls')),
+    # url(r'^about/', views.about, name='about'),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
